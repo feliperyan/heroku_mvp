363d090c2764746bccf2ef9ab34b19a5448e6c63
Felipe Ryan <feliperyan@gmail.com>
2012-12-07 22:03:47 +1100

    form is done

--- hello_app/views.py
+++ hello_app/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from hello_app.forms import *
 from django.http import HttpResponseRedirect
-
+from hello_app.models import *
 
 def index(request):
     if request.method == 'POST': # If the form has been submitted...
@@ -11,7 +11,18 @@ def index(request):
         
         if form.is_valid(): # All validation rules pass
             
-            
+            f = Feedback()
+
+            f.name = form.cleaned_data['name']
+            f.email = form.cleaned_data['email']
+            f.wantsServicesXchange = form.cleaned_data['wantsServicesXchange']
+            f.wantsGoodsXchange = form.cleaned_data['wantsGoodsXchange']
+            f.wantsCharityXchange = form.cleaned_data['wantsCharityXchange']
+            f.wantsLocationXchange = form.cleaned_data['wantsLocationXchange']
+            f.wantsBadges = form.cleaned_data['wantsBadges']
+            f.comments = form.cleaned_data['comments']
+
+            f.save()
 
             return HttpResponseRedirect('/thanks/') # Redirect after POST
     
